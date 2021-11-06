8c60d10c818f980bdfa0ecbe391c6665eb88d542
Chuei-Zuo Chen <louisa0103a@gmail.com>
2021-11-06 18:46:22 +0800

    use withTimeout() to automatically cancel job

--- app/src/main/java/com/example/kotlinandroidcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlinandroidcoroutines/MainActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import com.example.kotlinandroidcoroutines.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 
-const val TAG:String = "MainActivity"
+const val TAG: String = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,21 +16,46 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        //Do network call on IO thread, after get the result then update to Main thread UI views
-        GlobalScope.launch(Dispatchers.IO) {
-            Log.d(TAG, "Starting coroutine result in thread: ${Thread.currentThread().name}")
-            val result = doNetworkCall()
-            withContext(Dispatchers.Main){
-                Log.d(TAG, "Setting result in thread: ${Thread.currentThread().name}")
-                binding.tv1.text = result
+        /*val job = GlobalScope.launch(Dispatchers.Default) {
+            Log.d(TAG, "Starting long running calculation")
+            for (i in 30..40){
+                Log.d(TAG, "Result for $i = ${fib(i)}")
             }
+            Log.d(TAG, "Finished long running calculation")
+        }
+        //runBLocking it will block current thread, in here which is main thread,
+        //and it will block main thread until job is finished
+        runBlocking {
+            delay(2000L)
+            job.cancel()
+            Log.d(TAG, "Main Thread is continuing...")
+        }*/
+
+        //use withTimeout()
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            Log.d(TAG, "Starting long running calculation")
+            withTimeout(1000L) {
+                for (i in 30..50) {
+                    if (isActive) {
+                        Log.d(TAG, "Result for $i = ${fib(i)}")
+                    }
+                }
+            }
+            Log.d(TAG, "Finished long running calculation")
         }
     }
 
-    suspend fun doNetworkCall():String{
+    suspend fun doNetworkCall(): String {
         delay(3000L)
         return "doNetWorkResult"
+    }
+
+    fun fib(n: Int): Long {
+        return when (n) {
+            0 -> 0
+            1 -> 1
+            else -> fib(n - 1) + fib(n - 2)
+        }
     }
 
 }
